38a5cc75c9cfbc5fe9d400f3ff770a09c0c725e0
christianc2000 <christiancelsomamanisoliz0@gmail.com>
2023-06-15 17:18:34 -0400

    falta pago

--- src/databaseConection/dbconection.js
+++ src/databaseConection/dbconection.js
@@ -1,21 +1,21 @@
 require('dotenv').config();
 const { Client } = require('pg');
 
-/*const client = new Client({
+const client = new Client({
   user: process.env.PG_USER,
   host: process.env.PG_HOST,
   database: process.env.PG_DATABASE,
   password: process.env.PG_PASSWORD,
   port: process.env.PG_PORT,
-});*/
-const connectionString = 'postgres://postgresbancamovil:ytKbNChNhqT1bpRYWt8AWEC087KnGOZ8@dpg-ci4fgqtgkuvm71e2uu80-a.oregon-postgres.render.com/bancamovil';
+});
+/*const connectionString = 'postgres://postgresbancamovil:ytKbNChNhqT1bpRYWt8AWEC087KnGOZ8@dpg-ci4fgqtgkuvm71e2uu80-a.oregon-postgres.render.com/bancamovil';
 
 const client = new Client({
   connectionString,
   ssl: {
     rejectUnauthorized: false,
   },
-});
+});*/
 
 const conexion = async () => {
   try {
